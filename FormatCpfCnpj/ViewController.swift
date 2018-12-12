f787a310b1c90eea21d2db60a51f412239a426e8
Marcelo Sampaio <marcelo@artlogica.com,br>
2018-12-12 09:31:34 -0200

    checkpoint commit 🎒

--- FormatCpfCnpj/ViewController.swift
+++ FormatCpfCnpj/ViewController.swift
@@ -12,9 +12,60 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let input = "12345678901234"
+        
+        if input.count == 11 {
+            let cpfFormatted = self.cpfFormat(input)
+            print("👉 formatted cpf: \(cpfFormatted)")
+        }else if input.count == 14 {
+            let cnpjFormatted = self.cnpjFormat(input)
+            print("👉 formatted cnpj: \(cnpjFormatted)")
+        }
     }
 
+    // MARK: Formatter
+    private func cpfFormat(_ input: String) -> String {
+        var result = String()
+        var i = 1
+        var editChar = String()
+        for char in input {
+            if i == 3 || i == 6 {
+                editChar = "."
+            }
+            if i == 9 {
+                editChar = "-"
+            }
+            result = result + "\(char)" + editChar
+            i = i + 1
+            editChar = String()
+        }
+        
+        return result
+    }
 
+    private func cnpjFormat(_ input: String) -> String {
+        var result = String()
+        var i = 1
+        var editChar = String()
+        for char in input {
+            if i == 2 || i == 5 {
+                editChar = "."
+            }
+            if i == 8 {
+                editChar = "/"
+            }
+            if i == 12 {
+                editChar = "-"
+            }
+            result = result + "\(char)" + editChar
+            i = i + 1
+            editChar = String()
+        }
+        
+        return result
+    }
+    
+    
 }
 
